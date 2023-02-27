c7ee59919815bf003110e9af1d80b7527c3b1e7f
jonathan.cc.huang <jonathan.cc.huang@linecorp.com>
2023-02-28 00:49:29 +0800

    Rename util.rs to mod.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use serde_json::json;
 use std::sync::{Arc, Mutex};
 use warp::Filter;
 
-mod util;
+mod r#mod;
 
 #[tokio::main]
 async fn main() {
@@ -12,16 +12,16 @@ async fn main() {
 
     let config_builder = Config::builder().add_source(File::new("config.toml", FileFormat::Toml));
 
-    let channel_secret = util::get_channel_secret(config_builder);
+    let channel_secret = r#mod::get_channel_secret(config_builder);
 
     let parse_request_route = warp::post()
         .and(warp::path("verify"))
         .and(warp::header::<String>("x-line-signature"))
         .and(warp::body::bytes())
-        .and(util::with_channel_secret(Arc::new(Mutex::new(
+        .and(r#mod::with_channel_secret(Arc::new(Mutex::new(
             channel_secret,
         ))))
-        .and_then(util::parse_request_handler);
+        .and_then(r#mod::parse_request_handler);
 
     let test_route = warp::get()
         .and(warp::path("hello"))
